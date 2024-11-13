6020d90369a6baf5a88842c11973231614c4edd6
Aaron Mansfield <aaronmansfield12@gmail.com>
2024-11-13 00:52:51 +0000

    Bug patches

---
+++ config/index.ts
@@ -0,0 +1,15 @@
+export const BASE_URL = "https://ps99.biggamesapi.io/api/";
+
+export const HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Accept': 'application/json, text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/json'
+};
+
+export const DB_CONFIG = {
+    host: 'localhost',
+    user: 'root',
+    database: 'ps99_rap'
+};
